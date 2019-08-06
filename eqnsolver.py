3e54ca64b77933f24aa15d23e6d207f6600ddab8
Julian Bartels <jbartels@uni-bremen.de>
2019-08-06 17:53:28 +0200

    The project is started

---
+++ eqnsolver.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Routines for solving the one dimensional time independent schrodinger
+equation for any given potential."""
+import numpy as np
+
+
+def schrodinger():
+    print("trying to realize the algorithm here")
+
+
+def main():  # function object is only executed when the script is main prgrm
+    print("Main script")
+
+
+if __name__ == '__main__':
+    main()
