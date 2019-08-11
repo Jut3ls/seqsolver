8704a559c7a321d8a28f168952da9bf0a8650cec
Julian Bartels <jbartels@uni-bremen.de>
2019-08-11 21:13:54 +0200

    Modified seqsolver_io.py to understand in-line comments in input file.
    Modified .gitignore to ignore the potential.dat file

--- seqsolver_io.py
+++ seqsolver_io.py
@@ -1,22 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Main interface for reading and converting input data from schrodinger.inp
 """
 
 import sys
 
-# DIR = input("Please enter the directory of your schrodinger.inp file:")
-DIR = r"."
-try:
-    FP = open(DIR + r"\schrodinger.inp", "r")
-except FileNotFoundError:
-    print("Can not open file {}".format("schrodinger.inp"))
-    print("Exiting...")
-    sys.exit(1)
-else:
-    line = FP.readlines()
-    data = []  # created so that the file lines aren't overwritten
-    while line:
-        data = line
-        line = FP.readlines()
-    print(data)
+
+def main():
+    """Main script functionality"""
+
+    # directory = input("Please enter the directory of your input file:")
+    directory = r"."
+    try:
+        fp = open(directory + r"\schrodinger.inp", "r")
+    except FileNotFoundError:
+        print("Can not open file {}".format("schrodinger.inp"))
+        print("Exiting...")
+        sys.exit(1)
+    else:
+        lines = fp.readlines()
+        data = []
+        for i in range(len(lines)):
+            data.append(lines[i].split("#")[0])
+
+    fp.close()
+
+
+if __name__ == '__main__':
+    main()
